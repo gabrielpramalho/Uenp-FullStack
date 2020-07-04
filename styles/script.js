0aa0359b8df106010792a2d25f9c3055b5cbe480
GABRIEL PEDRO RAMALHO <50182891+gabrielpramalho@users.noreply.github.com>
2020-07-04 17:50:13 -0300

    add new pages and responsive

--- styles/script.js
+++ styles/script.js
@@ -1,13 +1,10 @@
 const buttomDark = document.querySelector('.buttom-dark a');
 const btnDark = document.querySelector('#btn');
-const contentPage = document.querySelector('.content p');
 const page = document.getElementById('pg')
 
 buttomDark.addEventListener('click', switchMode);
 
 var situationPage = 0;
-
-console.log(contentPage);
 
 function switchMode(){
     if(situationPage === 0){
